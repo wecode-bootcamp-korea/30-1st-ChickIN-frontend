342cc2903749933e65758cd28fbabd9012e38eea
sangmeen16 <sangmeen16@gmail.com>
2022-03-02 16:35:26 +0900

    Add : Goods_list page, Footer page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,29 @@
 function Footer() {
-  return <></>;
+  return <>
+  <div className="companyInfo">
+        <p>
+          <span className="companyName">ChickIN(주)</span>
+          <span className="classTitle">대표자</span>
+          <span>
+            <b>김지성 김가람휘 강성훈 이희수 이상민</b>
+          </span>
+        </p>
+        <p>
+          <span className="classTitle">개인정보 관리 책임자</span>
+          <span className="companyContent"> 김위코드</span>
+          <span className="classTitle">사업자 등록번호</span>
+          <span className="companyContent"> 123-44-56789</span>
+          <span className="classTitle">통신판매업 신고번호</span>
+          <span className="companyContent"> 2022-wecode-123456</span>
+        </p>
+        <p>
+          <span className="companyContent">서울특별시 강남구 테헤란로 427</span>
+          <span className="classTitle">팩스</span>
+          <span className="companyContent"> 02-1234-1234</span>
+          <span className="classTitle">호스팅</span>
+          <span className="companyContent"> 위코드</span>
+        </p>
+      </div></>;
 }
 
 export default Footer;
--- src/pages/Goods_list/Goods_list.js
+++ src/pages/Goods_list/Goods_list.js
@@ -1,5 +1,55 @@
 function Good_list() {
-  return <></>;
+  return (
+  <>
+  <div className="product_detail_container">
+        <div className="inner_wrap">
+          <div className="detail_top">
+            <div className="detail_left">
+              <div className="detail_image_container">
+                <div className="detail_image_wrapper">
+                  <img src="https://image.shutterstock.com/image-photo/chiken-potato-other-vegetables-on-600w-534962260.jpg" />
+                </div>
+              </div>
+            </div>
+            <div className="detail_right">
+              <div className="detail_information">
+                <div className="detail_information_title">
+                  닭가슴살 더블팩( 100g * 2팩 )
+                </div>
+                <div className="detail_information_wrapper">
+                  <div className="product_price_wrapper">
+                    <div className="product_discount"></div>
+                    <div className="product_new_price"></div>
+                  </div>
+                </div>
+                <div className="product-description"></div>
+                <div className="product_option">
+                  <div className="option_list">
+                    <select name="option">
+                      <option value="">옵션</option>
+                      <option value="cut">손질 (+2,000원)</option>
+                      <option value="ganish">가니쉬 (+2,000원)</option>
+                    </select>
+                  </div>
+                  <div className="option_total_area">
+                    <span className="product_name">닭가슴살 더블팩( 100g * 2팩 )</span>
+                    <div className="product_price">
+                      <span className="product_count">1</span>
+                      <em>
+                        <input type="hidden" value=""
+                      </em>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <!-- 댓글구현 --> */}
+          <div className="detail_bottom"></div>
+        </div>
+      </div>
+  </>
+  )
 }
 
 export default Good_list;
