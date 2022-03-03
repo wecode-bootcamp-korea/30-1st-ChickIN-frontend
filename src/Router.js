33c59767c14fcf089db3e8a355cc2f52f400978b
sangmeen16 <sangmeen16@gmail.com>
2022-03-03 21:29:52 +0900

    Add: Goods_view page proceeding

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main";
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Login from './components/Login/Login';
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default Router;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,15 @@
+import Footer from '../../components/Footer/Footer';
+import Goods_view from '../Goods_view/Goods_view';
+import items from '../Goods_view/mock.json';
 function Main() {
-  return <></>;
+  return (
+    <div className="body">
+      <Goods_view items={items} />
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default Main;
