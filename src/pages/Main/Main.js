66817990d7472daa793af5a1aab4f708f915b63c
sangmeen16 <sangmeen16@gmail.com>
2022-03-04 16:33:07 +0900

    Add : footer component

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,15 @@
+import Footer from '../../components/Footer/Footer';
 import GoodsView from '../Goods_view/GoodsView';
 import items from '../Goods_view/mock.json';
-
+import CounterBox from '../Goods_view/Counter_box/CounterBox';
 function Main() {
   return (
     <div className="body">
       <GoodsView items={items} />
+      <CounterBox />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
