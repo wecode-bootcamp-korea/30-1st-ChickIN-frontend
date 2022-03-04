8c2b777970fc0d9f351c256fac224617d806d6a2
Seonghun Kang <gceohoony@gmail.com>
2022-03-04 13:49:33 +0900

    Modify: Nav and  Footer in Router

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 import Main from './pages/Main';
 import GoodsList from './pages/Goods_list/GoodsList';
 import GoodsView from './pages/Goods_view/GoodsView';
@@ -9,6 +11,7 @@ import Signup from './components/Signup/Signup';
 function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/main" element={<Main />} />
         <Route path="/goodslist" element={<GoodsList />} />
@@ -16,6 +19,7 @@ function Router() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
