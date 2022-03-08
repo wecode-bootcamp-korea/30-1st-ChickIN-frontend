217fda872f134f87ad47aa7bac5529cbc4bc3037
Seonghun Kang <gceohoony@gmail.com>
2022-03-08 15:49:21 +0900

    Conflict solved

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
-import GoodsList from './pages/GoodsList/GoodsList';
-import GoodsView from './pages/GoodsView/GoodsView';
 import Login from './components/Login/Login';
 import Footer from './components/Footer/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
