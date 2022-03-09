9e982a978c68248dc7f20f8c9c5ec6871c6870a3
Heesoo Lee <heeesulee@Heesoos-MacBookPro.local>
2022-03-09 13:10:27 +0900

    Router.js 수정

--- src/Router.js
+++ src/Router.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
-import GoodsList from './pages/Goods_list/GoodsList';
-import GoodsView from './pages/Goods_view/GoodsView';
 import Login from './components/Login/Login';
-import Signup from './components/Signup/Signup';
+import Footer from './components/Footer/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/goodslist" element={<GoodsList />} />
-        <Route path="/goodsview" element={<GoodsView />} />
       </Routes>
       <Footer />
     </BrowserRouter>
   );
 }
-
 export default Router;
