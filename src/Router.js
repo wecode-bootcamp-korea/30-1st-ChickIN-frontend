ae452deb06f3b4edc4f8b6bb5887c39791c8d96d
Heesoo Lee <heeesulee@Heesoos-MacBookPro.local>
2022-03-09 13:08:45 +0900

    Router.js conflict 수정

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
 import Login from './components/Login/Login';
-// import Signup from './pages/Signup/Signup';
+import Footer from './components/Footer/Footer';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/signup" element={<Signup />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
   );
 }
-
 export default Router;
