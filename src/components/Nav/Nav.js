d1b288d7d25709ff5ac3fd368499b3a186695f6b
Seonghun Kang <gceohoony@gmail.com>
2022-03-10 20:41:16 +0900

    After merging master

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './Nav.scss';
 import {
   FaUser,
@@ -8,19 +7,7 @@ import {
 } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
-||||||| merged common ancestors
-import './Nav.scss';
-import {
-  FaUser,
-  FaMapMarkerAlt,
-  FaSistrix,
-  FaShoppingCart,
-} from 'react-icons/fa';
-
-=======
->>>>>>> master
 function Nav() {
-<<<<<<< HEAD
   const navigate = useNavigate();
   const goToDetail = path => {
     navigate(path);
@@ -99,80 +86,7 @@ function Nav() {
       </div>
     </nav>
   );
-||||||| merged common ancestors
-  return (
-    <nav className="nav">
-      <div className="nav_logo">
-        <a href="#">
-          <div>Chick.IN</div>
-        </a>
-      </div>
-      <div className="nav_service_menu">
-        <ul className="nav_service_list">
-          <li className="nav_service_item active">
-            <a href="#">구매</a>
-            <ul className="drop_down_menu">
-              <li className="meat_option">
-                <a href="#" className="chicken">
-                  닭고기
-                </a>
-                <ul className="drop_down_menu_under_chicken">
-                  <li>
-                    <a href="#">구이용</a>
-                  </li>
-                  <li>
-                    <a href="#">요리용</a>
-                  </li>
-                </ul>
-              </li>
-              <li className="meat_option">
-                <a href="#">오리고기</a>
-              </li>
-              <li className="meat_option">
-                <a href="#">특별한 구성</a>
-              </li>
-              <li className="meat_option">
-                <a href="#">치크인 ONLY</a>
-              </li>
-            </ul>
-          </li>
-
-          <li className="nav_service_item" data-link="#">
-            선물제안
-          </li>
-          <li className="nav_service_item" data-link="#">
-            설로인 매거진
-          </li>
-          <li className="nav_service_item" data-link="#">
-            이벤트
-          </li>
-          <li className="nav_service_item" data-link="#">
-            브랜드
-          </li>
-          <li className="nav_service_item" data-link="#">
-            B2B
-          </li>
-        </ul>
-      </div>
-      <div className="nav_user_menu">
-        <a href="#">
-          <FaMapMarkerAlt />
-        </a>
-        <a href="#">
-          <FaSistrix />
-        </a>
-        <a href="#">
-          <FaUser />
-        </a>
-        <a href="#">
-          <FaShoppingCart />
-        </a>
-      </div>
-    </nav>
-  );
-=======
   return <></>;
->>>>>>> master
 }
 
 export default Nav;
