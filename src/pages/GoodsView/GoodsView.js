f7d2623a3399887cd5a861493ed1f6c2eae09167
sangmeen16 <sangmeen16@gmail.com>
2022-04-29 14:50:05 +0900

    good-view id 주석

--- src/pages/GoodsView/GoodsView.js
+++ src/pages/GoodsView/GoodsView.js
@@ -20,7 +20,7 @@ function GoodsView() {
   const BASE_URL = 'http://13.125.170.124:8000';
   const navigate = useNavigate();
   const location = useLocation();
-  const id = location.search.split('=')[1];
+  // const id = location.search.split('=')[1];
 
   const CartButtonHandler = () => {
     fetch(`${BASE_URL}/carts/id`, {
