f33fe6390f30563562ff0f1b2fede9fac099adb9
sangmeen16 <sangmeen16@gmail.com>
2022-04-29 14:56:15 +0900

    config 수정

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
-const BASE_URL = 'http://10.58.6.148:8000';
+// const BASE_URL = 'http://10.58.7.79:8000';
+const BASE_URL = 'http://13.125.170.124:8000';
 export const API = {
   signup: `${BASE_URL}/users/signup`,
   login: `${BASE_URL}/users/login`,
   cart: `${BASE_URL}/carts`,
-  order: `${BASE_URL}/orders`,
-  goodsList: `${BASE_URL}/carts`,
-  goodsView: `${BASE_URL}/products/1`,
+  // goodsview: `${BASE_URL}/products`,
 };
